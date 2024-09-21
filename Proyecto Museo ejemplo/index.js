43fa869131adf8e226c23b9852d75e9f66e2bb43
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-09-21 12:54:41 -0300

    PegadoDeInicio

---
+++ Proyecto Museo ejemplo/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Translate } = require("node-google-translate-skidz");
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get("/traducir", (req, res) => {
+
+    const titulo = req.body.titulo;
+    const cultura = req.body.cultura;
+    const dinastia = req.body.dinastia;
+
+    translate({
+        text: req.params.texto,
+        source: 'en',
+        target: 'es'
+    }, function(result) {
+    res.json({ textoTraducido : result.translation });
+});
+});
+/*
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+*/
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
---
+++ Proyecto Museo ejemplo/public/scripts.js
@@ -0,0 +1,74 @@
+const URL_DEPARTAMENTOS = "https://collectionapi.metmuseum.org/public/collection/v1/departments";
+
+const URL_OBJETOS = "https://collectionapi.metmuseum.org/public/collection/v1/objects";  
+
+const URL_OBJETO = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+
+const URL_SEARCH_HAS_IMAGES = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=&hasImages=true";
+
+const URL_SEARCH = "https://collectionapi.metmuseum.org/public/collection/v1/search";
+
+function fetchDepartamentos() {
+    fetch(URL_DEPARTAMENTOS)
+    .then((response) => response.json())
+    .then((data) => {
+
+    const departamentoSelect = document.getElementById("departamento");
+    data.departments.forEach((departamento) => {
+    const option = document.createElement("option");
+    option.value = departamento.departmentId;
+    option.textContent = departamento.displayName;  
+    departamentoSelect.appendChild(option);  
+    });    
+});
+}
+    function fetchObjetos(objectIDs) {
+        let objetosHtml = "";
+        for (objectId of objectIDs) {
+            fetch(URL_OBJETO + objectId)
+            .then((response) => response.json())
+            .then((data) => {
+                objetosHtml += `<div class="objeto"> <img src="${
+                    data.primaryImageSmall != ""
+                    ? data.primaryImageSmall 
+                    : "sinimagen.png"
+                }" /> <h4 class="titulo"> ${data.title} </h4> <h6 class="cultura"> ${
+                    data.culture != "" ? data.culture : "Sin cultura"
+                } </h6> <h6 class="dinastia"> ${
+                    data.dynasty != "" ? data.dynasty : "Sin dinastia"
+                } </h6> </div>`;
+            document.getElementById("grilla").innerHTML = objetosHtml;
+            console.log(data.objectID);
+            });
+        }
+    }
+
+    fetchDepartamentos();
+
+    fetch(URL_SEARCH_HAS_IMAGES)
+    .then((response) => response.json())
+    .then((data) => {
+        fetchObjetos(data.objectIDs.slice(0,20));
+    });
+
+    document.getElementById("buscar").addEventListener("click", (event) => {
+        event.preventDefault();
+        const departamento = document.getElementById("departamento").value;
+        const keyword = document.getElementById("keyword").value;
+        const localizacion = document.getElementById("localizacion").value;
+
+        const paramLocalizacion = localizacion != "" ? `&geoLocalization=${localizacion}` : "";
+
+        console.log(URL_SEARCH + `?q=${keyword}&departmentId=${departamento}${paramLocalizacion}`)
+
+        fetch(
+
+            URL_SEARCH +
+             `?q=${keyword}&departmentId=${departamento}${paramLocalizacion}`
+        )
+             .then((response) => response.json())
+             .then((data) => {
+                fetchObjetos(data.objectIDs.slice(0,20));
+
+             });
+    });
