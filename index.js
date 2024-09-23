8c7767c99839bdfa1dbbe0ffb9ed365a0f1e332b
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-09-23 12:06:19 -0300

    .

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.get("/traducir", (req, res) => {
 });
 
 
-/*app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send('Hello World!');
 });
-*/
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
