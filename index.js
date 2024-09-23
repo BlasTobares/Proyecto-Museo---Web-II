880024268736b60665f119081d1ce417b30fcee6
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-09-23 13:19:35 -0300

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 /*const { Translate } = require("node-google-translate-skidz");*/
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
