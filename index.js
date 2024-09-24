4ebdf0bd350987b3a1101be077ed465ea7eb4c15
BlasTobares <163307394+BlasTobares@users.noreply.github.com>
2024-09-24 20:38:38 -0300

    .

--- index.js
+++ index.js
@@ -45,53 +45,21 @@ translate({
 });
 */
 
-async function traduccion(objetos) {
-    for (const element of objetos) {
-      const promises = [];
-  
-      if (element.title) {
-        let texto = element.title;
-        promises.push(
-          translate({
-            text: texto,
-            source: "en",
-            target: "es",
-          }).then((result) => {
-            element.title = result.translation;
-          })
-        );
-      }
-  
-      if (element.dynasty) {
-        let texto = element.dynasty;
-        promises.push(
-          translate({
-            text: texto,
-            source: "en",
-            target: "es",
-          }).then((result) => {
-            element.dynasty = result.translation;
-          })
-        );
-      }
-  
-      if (element.culture) {
-        let texto = element.culture;
-        promises.push(
-          translate({
-            text: texto,
-            source: "en",
-            target: "es",
-          }).then((result) => {
-            element.culture = result.translation;
-          })
-        );
-      }
-  
-      // todas las promesas de forma asincronica
-      await Promise.all(promises);
-    }
-  }
+app.post('/translate', (req, res) => {
+    const { text, targetLang } = req.body;
+
+    translate({
+        text: text,
+        source: 'en', // Idioma de origen (Inglés)
+        target: targetLang, // Idioma de destino (Español)
+    }, (result) => {
+        if (result && result.translation) {
+            res.json({ translatedText: result.translation });
+        } else {
+            res.status(500).json({ error: 'Error al traducir el texto' });
+        }
+    });
+});
 
 app.get("/", (req, res) => {
     res.send('Hello World!');
